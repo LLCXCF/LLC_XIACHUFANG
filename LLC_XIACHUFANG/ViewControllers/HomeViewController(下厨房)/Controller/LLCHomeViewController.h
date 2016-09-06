999e10f588e6c321d7408131487ea316a19d24bb
chendi <bbigcd@163.com>
2016-09-06 19:48:50 +0800

    created

---
+++ LLC_XIACHUFANG/ViewControllers/HomeViewController(下厨房)/Controller/LLCHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLCHomeViewController.h
+//  LLC_XIACHUFANG
+//
+//  Created by bbigcd on 16/9/6.
+//  Copyright © 2016年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLCHomeViewController : UIViewController
+
+@end
---
+++ LLC_XIACHUFANG/ViewControllers/HomeViewController(下厨房)/Controller/LLCHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  LLCHomeViewController.m
+//  LLC_XIACHUFANG
+//
+//  Created by bbigcd on 16/9/6.
+//  Copyright © 2016年 bbigcd. All rights reserved.
+//
+
+#import "LLCHomeViewController.h"
+
+@interface LLCHomeViewController ()
+
+@end
+
+@implementation LLCHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
